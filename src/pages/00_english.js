0d775b0fb2e516c4ec8cf5148caa6e9a2b3c4e88
Ola Vea <ola@olavea.com>
2019-01-22 10:42:14 +0100

    navigatin brute force

--- src/pages/00_english.js
+++ src/pages/00_english.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 import zero from '../images/forside_english_01.png'
-import Listing from '../components/Listing'
+// import Listing from '../components/Listing'
 import { Link } from 'gatsby'
 
 const About = () => (
@@ -17,7 +17,6 @@ const About = () => (
       <Link to="/"> Previous Page.../ </Link>
       <Link to="/01_english"> / Or go to the Next Page </Link>
     </div>
-    <Listing />
   </Layout>
 )
 
